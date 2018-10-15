9f12dc8275bf3e7970728c2dcd7ccb7bc89f578a
comunodi <alex_kote@bk.ru>
2018-10-16 02:24:17 +0300

    * fix calcDist * move read file in separate functions

--- src/Main.java
+++ src/Main.java
@@ -4,103 +4,106 @@ import java.util.*;
 
 
 public class Main {
+    private static int LIMIT = 3;
 
     public static void main(String[] args) {
-        HashSet<String> keyWords = new HashSet<>();
-        Scanner sc, sc1;
-        String line2 = "/Users/CO8ECTb/Desktop/Labs/SPL/3lab/3lab/main.c";
-        //Scanner in = new Scanner(System.in);
-        //String line1 = in.nextLine();
-        //char[] charArray = line1.toCharArray();
+        HashSet<String> keyWords = initKeyWords("src/KeyWords.txt");
+        List<String> content1 = parseFile("data/1.c", keyWords);
+        List<String> content2 = parseFile("data/2.c", keyWords);
+    }
 
-//        if(charArray[charArray.length-2] != '.' && charArray[charArray.length-1] != 'c'){
-//            System.out.println("not .c file");
-//            System.exit(1);
-//        }
-        File file1 = new File("src/KeyWords.txt");
-        File file = new File(line2);
-
-        try {
-            sc1 = new Scanner(file1);
-            while (sc1.hasNextLine()) {
-                String line = sc1.nextLine().trim();
-                keyWords.add(line);
-            }
-            sc1.close();
-        }
-        catch (FileNotFoundException e) {
-            e.printStackTrace();
-        }
-
+    private static HashSet<String> initKeyWords(String filename) {
+        HashSet<String> result = new HashSet<>();
+        Scanner sc;
+        File file = new File(filename);
         try {
             sc = new Scanner(file);
             while (sc.hasNextLine()) {
                 String line = sc.nextLine().trim();
-                if(line.contains("{") || line.contains("}")){
-                    line = line.replace("{","");
-                    line = line.replace("}","");
+                result.add(line);
+            }
+            sc.close();
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+        }
+        return result;
+    }
+
+    private static List<String> parseFile(String filename, HashSet<String> keyWords) {
+        List<String> result = new ArrayList<>();
+        Scanner sc;
+        File file = new File(filename);
+        try {
+            sc = new Scanner(file);
+            while (sc.hasNextLine()) {
+                String line = sc.nextLine().trim();
+                if (line.contains("{") || line.contains("}")) {
+                    line = line.replace("{", "");
+                    line = line.replace("}", "");
                 }
                 if (!line.isEmpty() && !line.contains("#include")) {
                     List<String> list = splitLineBySpace(splitByKey(line, keyWords));
                     StringBuilder newLine = new StringBuilder();
-                    for (int i=0; i< list.size();i++){
-                        if(!keyWords.contains(list.get(i))){
-                            if(!list.get(i).matches("[a-zA-Z]+")){
+                    for (int i = 0; i < list.size(); i++) {
+                        if (!keyWords.contains(list.get(i))) {
+                            if (!list.get(i).matches("[a-zA-Z]+")) {
                                 list.remove(i);
-                                list.add(i,"b");
-                            } else{
+                                list.add(i, "b");
+                            } else {
                                 list.remove(i);
-                                list.add(i,"a");
+                                list.add(i, "a");
                             }
                         }
-                        if(list.size() != (i+1)) newLine.append(list.get(i)).append(" ");
+                        if (list.size() != (i + 1)) newLine.append(list.get(i)).append(" ");
                         else newLine.append(list.get(i));
                         //System.out.println(i+" "+list.get(i));
                     }
-                    System.out.println(newLine);
-                    for (int i=0; i< list.size();i++){
-                        int y = calcDist(list.get(i),"")
-                    }
+                    //for (int i=0; i< list.size();i++){
+
+                    System.out.println(newLine.toString());
+                    int y = calcDist(newLine.toString(), "");
+                    System.out.println("dist = " + y);
+
+                    //}
                     System.out.println("--------------------");
 
+                    result.add(newLine.toString());
                 }
             }
-
             sc.close();
-
-        }
-        catch (FileNotFoundException e) {
+        } catch (FileNotFoundException e) {
             e.printStackTrace();
         }
+        return result;
     }
 
-    static List<String> splitLineBySpace(String line){
-                    String[] splittedLine = line.split("\\s+");
-                    List<String> list = new LinkedList<>();
-                    for(int i = 0; i < splittedLine.length; i++){
-                        if(!splittedLine[i].isEmpty()) {
-                            list.add(splittedLine[i]);
-                        }
-                    }
-                    return list;
-                }
-
-        static String splitByKey(String line, HashSet<String> keyWords){
-            System.out.println(line);
-
-            for (String key : keyWords) {
-                if (line.contains(key)) {
-                    //System.out.println("key = " +key);
-                    line = line.replace(key," "+key+" ");
-
-                }
+    static List<String> splitLineBySpace(String line) {
+        String[] splittedLine = line.split("\\s+");
+        List<String> list = new LinkedList<>();
+        for (String token : list) {
+            if (!token.isEmpty()) {
+                list.add(token);
             }
+        }
+        return list;
+    }
+
+
+    static String splitByKey(String line, HashSet<String> keyWords) {
+        System.out.println(line);
+
+        for (String key : keyWords) {
+            if (line.contains(key)) {
+                //System.out.println("key = " +key);
+                line = line.replace(key, " " + key + " ");
+
+            }
+        }
         return line;
     }
 
 
-
-     static int calcDist(String a, String b) {
+    static int calcDist(String a, String b) {
         if (a.length() > b.length()) {
             a = a + b;
             b = a.substring(0, (a.length() - b.length()));
@@ -112,7 +115,7 @@ public class Main {
         int cur = 1;
         for (int i = 0; i <= b.length(); ++i) {
             cur ^= 1;
-            for (int j = 0; j < a.length(); ++j) {
+            for (int j = 0; j <= a.length(); ++j) {
                 if (i == 0 || j == 0) {
                     dist[cur][j] = i > j ? i : j;
                 } else {
@@ -123,4 +126,9 @@ public class Main {
 
         return dist[cur][a.length()];
     }
+
+    static boolean areSameStrings(String a, String b) {
+        return calcDist(a, b) <= LIMIT;
+    }
+
 }
